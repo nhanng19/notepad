d9066fbf2392230c2c2e8137bffd2abc852d2de8
nhanng19 <nhanng19@gmail.com>
2022-09-23 18:23:42 -0700

    added commets for backend

--- server.js
+++ server.js
@@ -1,32 +1,52 @@
+// Require packages
+
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { request } = require("http");
+
+// Initialize app, port, and root
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const root = path.join(__dirname, "/public");
+
+// Middleware
 
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
 
+
+// Route for user to take notes
+
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(root, "notes.html"));
 });
 
+// Custom API route
+
 app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
+
+// API route for notes per id
 
 app.get("/api/notes/:id", (req, res) => {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     res.json(savedNotes[Number(req.params.id)]);
 });
 
+
+// Root route
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(root, "index.html"));
 });
+
+
+// POST requesest, add new note and object in database
 
 app.post("/api/notes", (req, res) => {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -39,6 +59,9 @@ app.post("/api/notes", (req, res) => {
     console.log("A new note has been saved: ", newNote);
     res.json(savedNotes);
 })
+
+
+// DELETE request, delete note and object in database
 
 app.delete("/api/notes/:id", (req, res) => {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
